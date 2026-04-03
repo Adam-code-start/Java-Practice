d464a9da6e9212a556953ac01ceb089b991619aa
Adam-code-start <saksham.sap6@gmail.com>
2026-04-03 20:50:35 +0530

    I could not access exercise 3 so I did exercise 4

---
+++ src/java_programming_I/Part_3/part_04/listSize.java
@@ -0,0 +1,23 @@
+package java_programming_I.Part_3.part_04;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class listSize {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+
+        while(true)
+        {
+            String name = sc.nextLine();
+            if(name.equals(""))
+            {
+                break;
+            }
+
+            names.add(name);
+        }
+
+        System.out.println(names.size());
+    }
+}
