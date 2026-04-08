1e74ec896e37e1d94324d08f8e20dffd5180408c
Adam-code-start <saksham.sap6@gmail.com>
2026-04-08 19:13:00 +0530

    Made a Person class from an example.

---
+++ src/java_programming_I/Part_4/Person.java
@@ -0,0 +1,6 @@
+package java_programming_I.Part_4;
+// a class named 'Person' is created
+public class Person {
+    String name;  // instance variables
+    int age; // another instance variable
+}
