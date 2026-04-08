96cf1a31e6a76061f5891c06fcc9e64cd84616e7
Adam-code-start <saksham.sap6@gmail.com>
2026-04-08 22:33:34 +0530

    I solved exercise 9

---
+++ src/java_programming_I/Part_4/part_09/Debt.java
@@ -0,0 +1,36 @@
+package java_programming_I.Part_4.part_09;
+
+public class Debt {
+    private double balance, interestRate;
+    public Debt(double initialBalance, double initialInterestRate)
+    {
+        balance = initialBalance;
+        interestRate = initialInterestRate;
+    }
+    public void printBalance()
+    {
+        System.out.println(balance);
+    }
+    public void waitOneYear()
+    {
+        balance *= interestRate;
+    }
+
+    public static void main(String[] args) {
+
+        Debt mortgage = new Debt(120000.0, 1.01);
+        mortgage.printBalance();
+
+        mortgage.waitOneYear();
+        mortgage.printBalance();
+
+        int years = 0;
+
+        while (years < 20) {
+            mortgage.waitOneYear();
+            years = years + 1;
+        }
+
+        mortgage.printBalance();
+    }
+}
